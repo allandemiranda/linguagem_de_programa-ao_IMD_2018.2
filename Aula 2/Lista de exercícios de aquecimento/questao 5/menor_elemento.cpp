1cd26a1eae0aee09f7ccdd4d62414cd3c009c85a
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-12 16:10:11 -0300

    arquivo questão 5 criado #5

---
+++ Aula 2/Lista de exercícios de aquecimento/questao 5/menor_elemento.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file menor_elemento.cpp
+ * @brief Questão 5 da lista
+ * @copyright Allan de Miranda - BTI - IMD - UFRN - 2018.2
+ **/
+#include <iostream> /*< Biblioteca usada para cout, cin e andl */
+#include <vector> /*< Biblioteca usada para vetores dinâmicos */
+
+/**
+ * @brief Função main
+ * @param void void
+ * @retunr 0
+ **/
+int main(void){
+
+    return 0;
+}
