7162413011a167a8b1baa1dba04084f79706c34e
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-12 16:05:44 -0300

    Novos comentários no código #4

--- Aula 2/Lista de exercícios de aquecimento/questao 4/fib_b4_n.cpp
+++ Aula 2/Lista de exercícios de aquecimento/questao 4/fib_b4_n.cpp
@@ -13,20 +13,24 @@ std::vector <int> sequencia_frib; /*< Quardará a sequencia de Fibonacci até o 
  * @param ate_numero A lista deve gerar núemros Fibonacci menores que este
  **/
 void criar_lista_frib(const int ate_numero){
-    sequencia_frib.clear();
+    //sequencia_frib.clear(); /*< Apagar dados se existirem no vetor */
+    /*! Adicionar os dois números iniciais */
     sequencia_frib.push_back(1);
     sequencia_frib.push_back(1);
-    int interacao(0);
+    int interacao(0); /*< contador de interações */
     while(true){
-        ++interacao;
+        ++interacao; /*< Próxima interação */
         int somar(0);
+        /*! Somar os dois números anteriores */
         for(int i(0); i<2; ++i){
             somar += sequencia_frib[interacao - i];
         }
+        /*! Caso tenha atingido o número limite */
         if(somar >= ate_numero){
             break;
-        }
-        sequencia_frib.push_back(somar);        
+        } else {
+            sequencia_frib.push_back(somar);
+        }             
     }
 }
 
@@ -37,13 +41,18 @@ void criar_lista_frib(const int ate_numero){
  **/
 int main(void){
     std::cout << "Digite número a analisar: " << std::endl;
-    int numero;
+    int numero; /*< Receber número digitado */
     std::cin >> numero;
-    criar_lista_frib(numero);
-    std::cout << "Números Fibonacci anteriores a este: " << std::endl;
-    for(int i : sequencia_frib){
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
+    if(numero>=1){
+        criar_lista_frib(numero); /*< Gerar lista */
+        std::cout << "Números Fibonacci anteriores a este: " << std::endl;
+        /*! Imprimir lista gerada*/
+        for(int i : sequencia_frib){
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "Não existem números Fibonacci anteriores a este" << std::endl;
+    }    
     return 0;
 }
