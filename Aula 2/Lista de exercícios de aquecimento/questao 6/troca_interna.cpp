b23109e22c34ef28a528c394ae3e1e7aaf2e990c
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-13 10:09:12 -0300

    desenvolvimento incial da questão 6 #6

---
+++ Aula 2/Lista de exercícios de aquecimento/questao 6/troca_interna.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file troca_interna.cpp
+ * @brief Questão 6 da lista
+ * @copyright Allan de Miranda - BTI - IMD - UFRN - 2018.2
+ **/
+#include <iostream> /*< Biblioteca usada para cout, cin e andl */
+#include <vector> /*< Biblioteca usada para vetores dinâmicos */
+
