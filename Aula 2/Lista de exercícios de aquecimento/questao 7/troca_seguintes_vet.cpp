75221a326f5ed3b8bd7455e1b57840f047c4d3be
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-13 19:10:29 -0300

    criação do arquivo para questão 7 #7

---
+++ Aula 2/Lista de exercícios de aquecimento/questao 7/troca_seguintes_vet.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file troca_seguinte_vet.cpp
+ * @brief Questão 6 da lista
+ * @copyright Allan de Miranda - BTI - IMD - UFRN - 2018.2
+ **/
+#include <iostream> /*< Biblioteca usada para cout, cin e andl */
+#include <algorithm> /*< Biblioteca usada para swap(a,b) -> (b,a) */
+
+#define  INTEIROS 20 /*< Quantidade de elementos a ser criados no vetor */
+
+/**
+ * @brief Função para receber o elementos do vetor
+ * @param Aponta para a posição inicial do vetor a ser usado na função
+ **/
+void receber_elementos(int *inicial){
+    for(int i(0); i<INTEIROS; ++i){
+        std::cin >> *(inicial + i);
+    }
+}
+
+/**
+ * @brief Função para imprimir todos os elementos do vetor
+ * @param Aponta para a posição inicial do vetor a ser usado na função
+ **/
+void imprimir_elementos(int *inicial){
+    for(int i(0); i<INTEIROS; ++i){
+        std::cout << *(inicial + i) << " ";
+    }
+    std::cout << std::endl;
+}
+
+/**
+ * @brief Função para trocar as posições dos elementos do vetor
+ * @param Aponta para a posição inicial do vetor a ser usado na função
+ **/
+void trocar_posicao(int *inicial){
+    for(int i(0), j(INTEIROS-1); i<j; ++i, --j){
+        std::iter_swap((inicial+i),(inicial+j));
+    }
+}
+
+/**
+ * @brief Função main
+ * @param void void
+ * @retunr 0
+ **/
+int main(void){
+    int A[INTEIROS];
+    std::cout << "Digite os " << INTEIROS << " elementos ao vetor: " << std::endl;
+    receber_elementos(&A[0]);
+    std::cout << "Lista de elementos adicionado: " << std::endl;
+    imprimir_elementos(&A[0]);
+    trocar_posicao(&A[0]);
+    std::cout << "Lista de elementos com posição trocada: " << std::endl;
+    imprimir_elementos(&A[0]);
+    return 0;
+}
