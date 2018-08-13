7c61bab57d70fb0cb88e43f51114f33323d91bec
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-13 20:20:45 -0300

    criação da lógica concluida

--- Aula 2/Lista de exercícios de aquecimento/questao 8/filter.cpp
+++ Aula 2/Lista de exercícios de aquecimento/questao 8/filter.cpp
@@ -4,6 +4,7 @@
  * @copyright Allan de Miranda - BTI - IMD - UFRN - 2018.2
  **/
 #include <iostream> /*< Biblioteca usada para cout, cin e andl */
+#include <iterator>  /*< Biblioteca usada para begin e end */
 
 int * filter(const * int first, const * int last){
     int *ponteiro_um = first;
@@ -11,10 +12,21 @@ int * filter(const * int first, const * int last){
     int *ponteiro_tres = last;
 
     bool flag(false);
-    while(true){
+    while(ponteiro_um != ponteiro_tres){
         if(flag){
             if(ponteiro_dois < 0){
                 ponteiro_dois++;
+                continue;
+            } else {
+                int *ponteiro_fake = ponteiro_um;
+                for(int i(0); (ponteiro_dois + i) != ponteiro_tres; ++i){
+                    *ponteiro_fake = *(ponteiro_dois + cont);
+                    ponteiro_fake++;
+                }                
+                ponteiro_tres = ponteiro_fake;
+                ponteiro_dois = ponteiro_um;
+                flag = false;
+                continue;
             }
         } else {
             if(*ponteiro_um < 0){
@@ -24,9 +36,11 @@ int * filter(const * int first, const * int last){
             } else {
                 flag = false;
                 ponteiro_um++;
+                continue;
             }
         }
     }
+    return ponteiro_tres;
 }
 
 /**
@@ -36,6 +50,11 @@ int * filter(const * int first, const * int last){
  **/
 int main(void){
     int vetor_teste[] = {-2, -8, 6, 7, -3, 10, 1, 0, -3, 7};
-
+    int *last;
+    last = filter(std::begin(vetor_teste), std:end(vetor_teste));
+    for(int i(0); (std::begin(vetor_teste) + i) != last; ++i){
+        std::cout << vetor_teste[i] << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
