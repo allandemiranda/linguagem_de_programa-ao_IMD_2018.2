ecacf73cc18a8b9863ce01be81bea9550c5b47f4
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-13 19:19:00 -0300

    cabeçalho questão 8 criado #8

---
+++ Aula 2/Lista de exercícios de aquecimento/questao 8/filter.cpp
@@ -0,0 +1,6 @@
+/**
+ * @file filter.cpp
+ * @brief Questão 8 da lista
+ * @copyright Allan de Miranda - BTI - IMD - UFRN - 2018.2
+ **/
+#include <iostream> /*< Biblioteca usada para cout, cin e andl */
