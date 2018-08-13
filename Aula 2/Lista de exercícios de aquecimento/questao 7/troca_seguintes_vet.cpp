c91025d8aa59d8518b0a36bc429acbbcd5fb1dca
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-13 19:11:33 -0300

    novas modificações de cabeçalho #7

--- Aula 2/Lista de exercícios de aquecimento/questao 7/troca_seguintes_vet.cpp
+++ Aula 2/Lista de exercícios de aquecimento/questao 7/troca_seguintes_vet.cpp
@@ -1,6 +1,6 @@
 /**
  * @file troca_seguinte_vet.cpp
- * @brief Questão 6 da lista
+ * @brief Questão 7 da lista
  * @copyright Allan de Miranda - BTI - IMD - UFRN - 2018.2
  **/
 #include <iostream> /*< Biblioteca usada para cout, cin e andl */
@@ -45,13 +45,13 @@ void trocar_posicao(int *inicial){
  * @retunr 0
  **/
 int main(void){
-    int A[INTEIROS];
+    int B[INTEIROS];
     std::cout << "Digite os " << INTEIROS << " elementos ao vetor: " << std::endl;
-    receber_elementos(&A[0]);
+    receber_elementos(&B[0]);
     std::cout << "Lista de elementos adicionado: " << std::endl;
-    imprimir_elementos(&A[0]);
-    trocar_posicao(&A[0]);
+    imprimir_elementos(&B[0]);
+    trocar_posicao(&B[0]);
     std::cout << "Lista de elementos com posição trocada: " << std::endl;
-    imprimir_elementos(&A[0]);
+    imprimir_elementos(&B[0]);
     return 0;
 }
