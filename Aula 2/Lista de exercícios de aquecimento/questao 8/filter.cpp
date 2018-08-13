161bee2f6aa7df4be4b452f67fa0cd1d7763563a
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-13 19:41:26 -0300

    desenvolvendo a função #8

--- Aula 2/Lista de exercícios de aquecimento/questao 8/filter.cpp
+++ Aula 2/Lista de exercícios de aquecimento/questao 8/filter.cpp
@@ -4,3 +4,38 @@
  * @copyright Allan de Miranda - BTI - IMD - UFRN - 2018.2
  **/
 #include <iostream> /*< Biblioteca usada para cout, cin e andl */
+
+int * filter(const * int first, const * int last){
+    int *ponteiro_um = first;
+    int *ponteiro_dois = first;
+    int *ponteiro_tres = last;
+
+    bool flag(false);
+    while(true){
+        if(flag){
+            if(ponteiro_dois < 0){
+                ponteiro_dois++;
+            }
+        } else {
+            if(*ponteiro_um < 0){
+                ponteiro_dois = ponteiro_um + 1;
+                flag = true;
+                continue;
+            } else {
+                flag = false;
+                ponteiro_um++;
+            }
+        }
+    }
+}
+
+/**
+ * @brief Função main
+ * @param void void
+ * @retunr 0
+ **/
+int main(void){
+    int vetor_teste[] = {-2, -8, 6, 7, -3, 10, 1, 0, -3, 7};
+
+    return 0;
+}
