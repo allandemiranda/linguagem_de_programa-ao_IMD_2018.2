043432a5f816590f0a9fe5f6fb96cf974d11bc8a
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-13 10:02:10 -0300

    questao 5 finalizada #5

--- Aula 2/Lista de exercícios de aquecimento/questao 5/menor_elemento.cpp
+++ Aula 2/Lista de exercícios de aquecimento/questao 5/menor_elemento.cpp
@@ -24,13 +24,32 @@ void preencher_vetor(void){
  * @param final_p Aponta para posição final do vetor
  * @return Retorna um ponteiro da posição do menor elemento encontrado primeiro
  **/
-int * verificador_menor(int *inicial_p, int *final_p){
+int * verificador_valor_menor(int * const inicial_p, int const *final_p){
     int valor = *inicial_p;
     int *posicao = inicial_p;
     for(int i(1); i<(final_p-inicial_p); ++i){
         if(valor>*(inicial_p+i)){
             valor = *(inicial_p + i);
             posicao = (inicial_p + i);
+        }
+    }
+    return posicao;
+}
+
+/**
+ * @brief Função para verificar qual a posição do menor elemento
+ * @param inicial_p Ponteiro que aponta para o primeiro elemento do vetor
+ * @param posicao_memoria Posição da memoria do menor valor
+ * @return Retorna a posição no vetor que é ocupada pela posicao_memoria 
+ **/
+int verificar_posicao_menor(int const *inicial_p, int * const posicao_memoria){
+    int posicao(0);
+    while(true){
+        if(inicial_p==posicao_memoria){
+            break;
+        } else {
+            inicial_p++;
+            ++posicao;
         }
     }
     return posicao;
@@ -45,7 +64,8 @@ int main(void){
     std::cout << "Adicione números ao vetor: " << std::endl;
     /*! receber n números */
     preencher_vetor(); /*< Iniciar preencimento do vetor */
-    std::cout << "Menor valor: " << *(verificador_menor(&Vet.front(), &Vet.back()+1)) << std::endl;
-    std::cout << "Posição: " << (verificador_menor(&Vet.front(), &Vet.back()+1)) << std::endl;
+    int *menor_valor = (verificador_valor_menor(&Vet.front(), &Vet.back()+1));
+    std::cout << "Menor valor: " << *menor_valor << std::endl;
+    std::cout << "Posição: " << verificar_posicao_menor(&Vet.front(), menor_valor) << std::endl;
     return 0;
 }
