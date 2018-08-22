2e87d6e0dc133d3e732aeda53c81f7df9889cb9f
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-22 14:15:31 -0300

    questão 2 implementada

---
+++ Aula 6/questao 2/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file main.cpp
+ * @brief Questão 1 da lista
+ * @copyright Allan de Miranda - BTI - IMD - UFRN - 2018.2
+ */
+#include <iostream> // std::cout , std::cin 
+#include <iterator> // std::begin() , std::end();
+
+/**
+ * @brief retorna um ponteiro para a primeira ocorrência do menor elemento no intervalo
+ * 
+ * @param first Aponta para o primeiro elemento a analisar
+ * @param last Aponta para o elemento seguinte ao últimmo a ser analisado
+ * @return const int* retorna o ponteiro para o menor inteiro do intervalo analisado
+ */
+const int * min(const int *first, const int *last){
+    const int *menor(first); //*< Ponteiro com a ponisão do menor elemento analisado */
+    for(const int *i(first+1); i<last; ++i){ // Para analisar o intervalo dado
+        if(*i<*menor){ // Se menor que o menor já determinado
+            menor = i; // Modifique a posição para a nova posição
+        }
+    }
+    return menor;
+}
+
+int main(void){
+    int A[] = {1, 2, -3, -4, 5, -6};
+
+    auto result = min(std::begin(A), std::end(A)); // resposta = (-6)
+    std::cout << *result << std::endl;
+
+    result = min(std::begin(A), std::begin(A)+5); // resposta = (-4)
+    std::cout << *result << std::endl;
+
+    return 0;
+}
