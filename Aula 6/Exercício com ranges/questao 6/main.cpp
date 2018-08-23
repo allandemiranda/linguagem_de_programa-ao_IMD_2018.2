7443a269ad6094018b397e85c3cfc2840ff652b1
Allan de Miranda Silva <allan94@A307-20L.imd.local>
2018-08-23 10:38:49 -0300

    brok questão 6

--- Aula 6/Exercício com ranges/questao 6/main.cpp
+++ Aula 6/Exercício com ranges/questao 6/main.cpp
@@ -6,6 +6,44 @@
 #include <iostream> // std::cout , std::cin 
 #include <iterator> // std::begin() , std::end(), std::distance()
 
+int * compact(int *first, int *last){
+    int *fast = first;
+    for(int *i(first); i<last; ++i){
+        if(fast==last){
+            return i;
+        }
+        if(*i < 0){
+            while(true){
+                if(fast<i){
+                    fast = i;
+                } 
+                fast++;
+                if(fast==last){
+                    break;
+                } else {
+                    if(*fast > 0){
+                        *i = *fast;
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                
+            }
+        }        
+    }
+}
+
 int main(void){
-    
+    int A[] = {-2, -8, 2, 7, -3, 10, 1, 0, -3, 7};
+    int * posicao = compact(std::begin(A), std::end(A));
+
+    for(int *i(std::begin(A)); i<std::end(A); ++i){
+        if(i==posicao){
+            std::cout << "(" << *i << ") " << std::endl;
+        } else {
+            std::cout << *i << " ";
+        }
+    }
+    std::cout << std::endl;    
 }
