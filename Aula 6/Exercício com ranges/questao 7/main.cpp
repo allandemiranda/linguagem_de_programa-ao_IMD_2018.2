d088b67640a0d2a2bc1f8619ec193d5ee30e3790
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-27 17:04:08 -0300

    novas modificações questão 7

--- Aula 6/Exercício com ranges/questao 7/main.cpp
+++ Aula 6/Exercício com ranges/questao 7/main.cpp
@@ -6,6 +6,30 @@
 #include <iostream> // std::cout , std::cin 
 #include <iterator> // std::begin() , std::end(), std::distance()
 
+/**
+ * @brief Função que copia os elementos no intervalo para outro intervalo iniciando
+ * 
+ * @param first Ponteiro que aponta para primeira posição do vetor a ser copiado
+ * @param last Ponteiro que aponta para a posição seguinte do vetor a ser copiado
+ * @param d_first Ponteiro que aponta para primeira posição do vetor que receberá a cópia
+ * @return int* Retorna um ponteiro para a posição seguinte ao vetor copiado
+ */
+int * copy(const int *first, const int *last, int *d_first){
+
+}
+
 int main(void){
+    int A[] = {1,2,3,4,5,}; // Vetor "fonte"
+    int B[] = {0,0,0}; // Vetor "destino"
+
+    // Copiar elementos 2, 3, e 4 para o início de B.
+    auto b_last = copy(std:begin(A)+1, std::begin(A)+4, std::begin(B));
+
+    // O comando abaixo deveria imprimir B com o conteudo 2, 3, 4.
+    for(auto i(std::begin(B)); i != b_last; ++i){
+        std::cout << *i << " ";
+    }
+    std::cout << std::endl;
+    
     return 0;
 }
