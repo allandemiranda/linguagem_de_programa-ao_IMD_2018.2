1ea7b6bbf41196566d3855a3e4126c3402678022
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-27 16:49:05 -0300

    questão 6 finalizada

--- Aula 6/Exercício com ranges/questao 6/main.cpp
+++ Aula 6/Exercício com ranges/questao 6/main.cpp
@@ -6,44 +6,49 @@
 #include <iostream> // std::cout , std::cin 
 #include <iterator> // std::begin() , std::end(), std::distance()
 
-int * compact(int *first, int *last){
-    int *fast = first;
-    for(int *i(first); i<last; ++i){
-        if(fast==last){
-            return i;
+/**
+ * @brief Função para "compacta" os elementos no intervalo
+ * 
+ * @param first Aponta para primeiro elemento do vetor
+ * @param last Aponta para posição sequinte a última posição do vetor
+ * @return int* Retorna um ponteioro para posição seguinte da compactação
+ */
+int * compact(int * const first, int * const last){    
+    int *fast = first; /*< Ponteiro que caminhará sobre o vetor de forma rápida */ 
+    for(int *slow = first; slow <= last; ++slow){ /*< Percorra todo o vetor */                 
+        if(slow == last){ /*< Verificar se a compactação chegou ao fim */
+            return slow;
         }
-        if(*i < 0){
-            while(true){
-                if(fast<i){
-                    fast = i;
-                } 
+        while(true){ /*< Buscar próximo positivo apra compactação */
+            if(fast == last){ /*< Verificar se a compactação chegou ao fim */
+                return slow;
+            }            
+            if(*fast > 0){
+                *slow = *fast;
                 fast++;
-                if(fast==last){
-                    break;
-                } else {
-                    if(*fast > 0){
-                        *i = *fast;
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-                
-            }
-        }        
+                break;                
+            } else {
+                fast++;                
+            }            
+        } 
     }
 }
 
 int main(void){
-    int A[] = {-2, -8, 2, 7, -3, 10, 1, 0, -3, 7};
-    int * posicao = compact(std::begin(A), std::end(A));
+    int A[] = {-2, -8, 2, 7, -3, 10, 1, 0, -3, 7}; /*< Vetor teste */
+    int * posicao = compact(std::begin(A), std::end(A)); /*< Compactar */
 
-    for(int *i(std::begin(A)); i<std::end(A); ++i){
-        if(i==posicao){
-            std::cout << "(" << *i << ") " << std::endl;
-        } else {
-            std::cout << *i << " ";
-        }
+    // Números ordenados
+    for(int *i(std::begin(A)); i<posicao; ++i){
+        std::cout << *i << " ";
+    }  
+    std::cout << std::endl;
+
+    // Parte do vetor não ordenada
+    for(int *i(posicao); i<std::end(A); ++i){
+        std::cout << *i << " ";
     }
-    std::cout << std::endl;    
+    std::cout << std::endl;
+
+    return 0;
 }
