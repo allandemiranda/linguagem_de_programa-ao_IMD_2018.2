3b066d240abd6b034ee66d7cc527b77e3a18bb49
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-27 16:55:59 -0300

    inciando a questão 7

---
+++ Aula 6/Exercício com ranges/questao 7/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file main.cpp
+ * @brief Questão 7 da lista
+ * @copyright Allan de Miranda - BTI - IMD - UFRN - 2018.2
+ */
+#include <iostream> // std::cout , std::cin 
+#include <iterator> // std::begin() , std::end(), std::distance()
+
+int main(void){
+    return 0;
+}
