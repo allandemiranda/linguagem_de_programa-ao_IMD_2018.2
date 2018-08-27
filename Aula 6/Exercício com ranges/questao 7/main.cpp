5dc287c75e47d3e05bdb26e169c4b5744a6673c8
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-08-27 17:11:30 -0300

    questão 7 finalizada

--- Aula 6/Exercício com ranges/questao 7/main.cpp
+++ Aula 6/Exercício com ranges/questao 7/main.cpp
@@ -12,10 +12,13 @@
  * @param first Ponteiro que aponta para primeira posição do vetor a ser copiado
  * @param last Ponteiro que aponta para a posição seguinte do vetor a ser copiado
  * @param d_first Ponteiro que aponta para primeira posição do vetor que receberá a cópia
- * @return int* Retorna um ponteiro para a posição seguinte ao vetor copiado
+ * @return int* Retorna um ponteiro para a posição final do vetor copiado
  */
 int * copy(const int *first, const int *last, int *d_first){
-
+    for(auto *i(first); i<last; ++i, ++d_first){ /*< percorrer todo o vetor */
+        *d_first = *i;
+    }
+    return d_first;
 }
 
 int main(void){
@@ -23,13 +26,13 @@ int main(void){
     int B[] = {0,0,0}; // Vetor "destino"
 
     // Copiar elementos 2, 3, e 4 para o início de B.
-    auto b_last = copy(std:begin(A)+1, std::begin(A)+4, std::begin(B));
+    auto b_last = copy(std::begin(A)+1, std::begin(A)+4, std::begin(B));
 
     // O comando abaixo deveria imprimir B com o conteudo 2, 3, 4.
     for(auto i(std::begin(B)); i != b_last; ++i){
         std::cout << *i << " ";
     }
     std::cout << std::endl;
-    
+
     return 0;
 }
