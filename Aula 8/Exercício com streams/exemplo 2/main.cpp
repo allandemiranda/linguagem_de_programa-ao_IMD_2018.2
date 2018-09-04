30f78184c32f845a406eb38872e44e68a8dfb111
Allan de Miranda <allandemiranda@gmail.com>
2018-09-04 10:20:56 -0300

    novo exemplo adicionado

---
+++ Aula 8/Exercício com streams/exemplo 2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string askUserFileName(std::ifstream & ifs_, std::string msg = ""){
+    std::string fileName;
+    while(true){
+        std::cout << msg_;
+        std::getline(std::cin, fileName); // Get the file name .
+        ifs_.open(fileName.c_str()); // Try to open the file .
+        if(! ifs_.fail()){
+            return fileName;
+        }
+        fileName.clear(); // Clear file name , try again ...
+        std::cout << "Unable to open the file . Try again .\ n";
+        if(msg_ == ""){
+            msg_ = "Enter file name: ";
+        }
+    }
+}
+
+int main(void){
+    std::ifstream ifs;
+    askUserFileName(ifs, "Please , enter a file name :");
+    char ch;
+    while(ifs.get(ch)){
+        std::cout.put(ch);
+    }
+    ifs.close();
+
+    return EXIT_SUCCESS;
+}
