ea8bc9eef891923cb0b4cd8d2385adb304f80ab1
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-09-04 08:28:53 -0300

    novas modificações na questão 1 #20

--- Aula 8/Exercício com streams/exercicio 1/main.cpp
+++ Aula 8/Exercício com streams/exercicio 1/main.cpp
@@ -1,12 +1,41 @@
-#include <iostream>
+/**
+ * @brief Exercício 1 - Exercícios com streams
+ * 
+ * @file main.cpp
+ * @author Allan de Miranda Silva
+ * @date 2018-09-04
+ */
+#include <iostream> // std::cout , std::cin 
 #include <iomanip>
-#include <vector>
+#include <vector> // std::vector
 
+/**
+ * @brief Função para criação do cabeçalho
+ * 
+ * @param fields_ Palavras que irão compor o cabeçalho das colunas da tabela
+ */
 void printHeader(const std::vector <std::string> & fields_){
-	// contar quantidade de caracteres de cada palavra passado para a função
-		//PROBLEMA POIS TEM O CARACTER DE TRANSIÇÃO
-			//TENTAR USAR ALGUMA COISA PARA IMPRIMIR QUANTIDADE SEM IMPRIMIR A PALAVRA
-	// adicionar caracteres finais e de transição de tabelaa quantidade de letaas
+	// Primeira linha
+	std::cout << "+";
+	for(auto i : fields_){
+		std::cout << "-";
+		for(auto j(0); j<i.size(); ++j){
+			std::cout << "-";
+		}
+		std::cout << "-+"; 
+	}
+
+	// Segunda linha
+	
+
+	// Calcular largura da tabela
+	auto largura_tabela(0); // Largura da tabela
+	for(auto i : fields_){
+		largura_tabela += 2; // Antes da Palavra
+		largura_tabela += i.size(); // A Palavra
+		largura_tabela += 2; // Depois da Palavra
+	}
+	
 }
 
 int main(void){
